7c3e23b91e0f4ed7e2bdba82759343f1d78c42f6
Kirill Lischener <83303079+LischenerKS@users.noreply.github.com>
2025-12-14 21:13:12 +0300

    v2.0 release

---
+++ input_validator.py
@@ -0,0 +1,149 @@
+import os
+from typing import Optional
+
+from PIL import Image
+
+
+class InputValidator:
+    """
+    Класс для проверки переданных программе аргументов
+    """
+
+    def __init__(
+        self,
+        path_to_image_base_for_mosaic: str,
+        path_to_images: str,
+        path_to_output_image_dir: str,
+        size_of_replaced_pixel: str,
+        width_of_output_image: str,
+        height_of_output_image: str,
+    ):
+        self.path_to_imagebase_for_mosaic = path_to_image_base_for_mosaic
+        self.path_to_images = path_to_images
+        self.path_to_output_image_dir = path_to_output_image_dir
+        self.size_of_replaced_pixel = size_of_replaced_pixel
+        self.width_of_output_image = width_of_output_image
+        self.height_of_output_image = height_of_output_image
+
+    def check_args(self) -> None:
+        """
+        Вызывает методы для проверки self.args
+        Если аргумент(ы) некорректны, то выводит пользователю какие из аргументов некорректны
+        и останавливает выполнение всей программы.
+        """
+        incorrect_args = [
+            self._check_path_to_imagebase_for_mosaic(),
+            self._check_path_to_images_dir(),
+            self._check_path_to_output_image_dir(),
+            self._check_size_of_replaced_pixel(),
+            self._check_width_of_output_image(),
+            self._check_height_of_output_image(),
+        ]
+
+        is_given_incorrect_args = False
+
+        for inc_arg in incorrect_args:
+            if inc_arg is not None:
+                is_given_incorrect_args = True
+                print(f"Аргумент {inc_arg} введен неверно!")
+
+        if is_given_incorrect_args:
+            print("Попробуйте еще раз.")
+            exit()
+
+    def _check_path_to_imagebase_for_mosaic(self) -> Optional[str]:
+        """
+        Пытается открыть указанный в self.path_to_imagebase_for_mosaic файл как изображение.
+        Если файл не удалось открыть, то возвращает
+        строку 'path_to_imagebase_for_mosaic'.
+        Если файл удалось открыть, то возвращает None
+        """
+        try:
+            Image.open(self.path_to_imagebase_for_mosaic).close()
+            return None
+        except:
+            return "path_to_imagebase_for_mosaic"
+
+    def _check_path_to_images_dir(self) -> Optional[str]:
+        """
+        Пытается,
+        прочитать список имен по полученному из path_to_images пути
+        и открыть каждое из списка как изображение.
+        Если хотя бы один файл не удалось открыть, то возвращает строку 'path_to_images'.
+        Если каждый файл удалось открыть, то возвращает None
+        """
+        try:
+            image_names = os.listdir(self.path_to_images)
+            for image_name in image_names:
+                path = os.path.join(self.path_to_images, image_name)
+                Image.open(path).close()
+            return None
+        except:
+            return "path_to_images"
+
+    def _check_path_to_output_image_dir(self) -> Optional[str]:
+        """
+        Пытается получить path_to_output_image_dir путь до директории
+        для сохранения полученного изображения,
+        и проверить что директория существует.
+        Если директория не существует, то создает ее
+        Если создать или открыть директорию не удается, то
+        возвращает строку 'path_to_output_image_dir'
+        """
+        try:
+            if not os.path.isdir(self.path_to_output_image_dir):
+                os.mkdir(self.path_to_output_image_dir)
+                return None
+            return None
+        except:
+            return "path_to_output_image_dir"
+
+    def _check_size_of_replaced_pixel(self) -> Optional[str]:
+        """
+        Пытается получить из size_of_replaced_pixel размер пикселя для замены
+        и проверить что это целое положительное число
+        Если проверка не выполняется или оказывается ложной, то возвращает строку 'size_of_replaced_pixel'.
+        Иначе возвращает None
+        """
+        try:
+            if not (int(self.size_of_replaced_pixel) > 0):
+                return "size_of_replaced_pixel"
+            return None
+        except:
+            return "size_of_replaced_pixel"
+
+    def _check_width_of_output_image(self) -> Optional[str]:
+        """
+        Пытается получить из self.width_of_output_image ширину итогового изображения
+        и проверить что она является целым положительным числом или None.
+        Если проверка не выполняется или оказывается ложной, то возвращает строку 'width_of_output_image'.
+        Иначе возвращает None
+        """
+        try:
+            if (
+                self.width_of_output_image is None
+                or int(self.width_of_output_image) > 0
+            ):
+                return None
+            else:
+                return "width_of_output_image"
+        except:
+            return "width_of_output_image"
+
+    def _check_height_of_output_image(self) -> Optional[str]:
+        """
+        Пытается получить из self.height_of_output_image высоту итогового изображения
+        и проверить что она является целым положительным числом или None.
+        Если проверка не выполняется или оказывается ложной, то возвращает строку 'height_of_output_image'.
+        Иначе возвращает None
+        """
+        try:
+            if (
+                self.height_of_output_image is None
+                or int(self.height_of_output_image) > 0
+            ):
+                return None
+            else:
+                return "height_of_output_image"
+        except:
+            return "height_of_output_image"
---
+++ mosaic.py
@@ -0,0 +1,188 @@
+from PIL import Image
+from tqdm import tqdm
+
+from args_parsers import ArgsParserFabric
+
+
+from environments import MODEL_KEY
+from errors import ClosestColorNotFoundError
+from image_loader import ImageLoader
+from image_saver import ImageSaverFabric
+
+
+class MosaicCreator:
+    """
+    класс для генерации мозаики
+    """
+
+    def __init__(self, images, images_width, images_height):
+        self._images = images
+        self._avg_colors_images = self._get_avg_colors_array(self._images)
+
+        self._images_width = images_width
+        self._images_height = images_height
+
+    def _get_avg_color_image(self, image) -> tuple[int, int, int]:
+        """
+        метод возвращающий для изображения его средний цвет
+        """
+        r = 0
+        g = 0
+        b = 0
+        count_pixels = image.width * image.height
+
+        for width in range(image.width):
+            for height in range(image.height):
+                pixel = image.getpixel((width, height))
+                r += pixel[0]
+                g += pixel[1]
+                b += pixel[2]
+
+        r //= count_pixels
+        g //= count_pixels
+        b //= count_pixels
+
+        return r, g, b
+
+    def _get_avg_colors_array(self, images) -> list[tuple[int, int, int]]:
+        """
+        метод возвращающий для self.images массив средних цветов
+        """
+        avg_colors = []
+
+        for image in images:
+            avg_colors.append(self._get_avg_color_image(image))
+
+        return avg_colors
+
+    def _get_distance_between_pixels(self, pixel_1, pixel_2) -> int:
+        delta_r = abs(pixel_1[0] - pixel_2[0])
+        delta_g = abs(pixel_1[1] - pixel_2[1])
+        delta_b = abs(pixel_1[2] - pixel_2[2])
+        return delta_r + delta_b + delta_g
+
+    def _find_index_of_closest_by_avg_color_image(
+        self, pixel, avg_colors_images
+    ) -> int:
+        """
+        находит в avg_colors_images ближайшее по среднему цвету изображение
+        к переданному pixel и возвращает его index
+        """
+
+        min_delta = 10**10
+        result_id = -1
+
+        for i in range(len(avg_colors_images)):
+            distance = self._get_distance_between_pixels(pixel, avg_colors_images[i])
+
+            if distance < min_delta:
+                min_delta = distance
+                result_id = i
+
+        if result_id == -1:
+            raise ClosestColorNotFoundError(pixel)
+
+        return result_id
+
+    def create_and_show_mosaic_image(self, old_image) -> Image:
+        """
+        создает изображение new_image, где пиксели в old_image заменены
+        на ближайшие по среднему цвету изображения из images
+        и возвращает его
+        """
+
+        new_image_pixels = []
+        """
+        для i-го пикселя старого изображения хранит id
+        ближайшего по среднему цвету изображения в _images
+        """
+
+        new_image_width = old_image.width * self._images_width
+        new_image_height = old_image.height * self._images_height
+
+        for h in tqdm(
+            range(old_image.height),
+            desc="Готовим изображения для замены пикселей",
+            leave=False,
+            colour="MAGENTA",
+        ):
+            for w in range(old_image.width):
+                pixel = old_image.getpixel((w, h))
+                closest_image_ids = self._find_index_of_closest_by_avg_color_image(
+                    pixel, self._avg_colors_images
+                )
+                new_image_pixels.append(closest_image_ids)
+
+        new_image = Image.new("RGB", (new_image_width, new_image_height))
+
+        image_id = 0
+        for h in tqdm(
+            range(old_image.height),
+            desc="Создаем новое изображение",
+            leave=False,
+            colour="green",
+        ):
+            for w in range(old_image.width):
+                new_w = w * self._images_width
+                new_h = h * self._images_height
+                image_to_replace_pixel = self._images[new_image_pixels[image_id]]
+                new_image.paste(image_to_replace_pixel, (new_w, new_h))
+                image_id += 1
+
+        return new_image
+
+
+class MosaicFacade:
+    def create_mosaic(self, args_source: str) -> None:
+        parser_fabric = ArgsParserFabric()
+        argparser = parser_fabric.get_parser(args_source)
+
+        program_args = argparser.get_args_dictionary()
+
+        image_loader = ImageLoader(program_args["path_to_images"])
+
+        path_to_imagebase_for_mosaic = program_args["path_to_imagebase_for_mosaic"]
+        base_image = image_loader.get_image_by_path(path_to_imagebase_for_mosaic)
+
+        if program_args["width_of_output_image"] is None:
+            program_args["width_of_output_image"] = (
+                MODEL_KEY["DEFAULT_SCALE_MULTIPLIER"] * base_image.width
+            )
+
+        if program_args["height_of_output_image"] is None:
+            program_args["height_of_output_image"] = (
+                MODEL_KEY["DEFAULT_SCALE_MULTIPLIER"] * base_image.height
+            )
+
+        base_image = base_image.resize(
+            (
+                program_args["width_of_output_image"],
+                program_args["height_of_output_image"],
+            )
+        )
+
+        images_with_names = image_loader.get_images_with_names()
+
+        width_of_replaced_pixel = program_args["size_of_replaced_pixel"]
+        height_of_replaced_pixel = program_args["size_of_replaced_pixel"]
+
+        image_loader.resize_images(
+            images_with_names[1], width_of_replaced_pixel, height_of_replaced_pixel
+        )
+
+        my_mosaic_creator = MosaicCreator(
+            images_with_names[1],
+            width_of_replaced_pixel,
+            height_of_replaced_pixel,
+        )
+
+        output_image = my_mosaic_creator.create_and_show_mosaic_image(base_image)
+
+        my_image_saver_fabric = ImageSaverFabric()
+        my_image_saver = my_image_saver_fabric.get_input_saver(
+            input_save_type="dir",
+            path_to_output_image=program_args["path_to_output_image_dir"],
+        )
+        print(
+            f"Генерация успешно завершена, путь до изображения {my_image_saver.save(output_image)}"
+        )
